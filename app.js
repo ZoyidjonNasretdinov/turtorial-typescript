edd8cab2e16286bdffea10c204fea5c336fec251
ZoyidjonNasretdinov <zoyidjonnasretdinovcoder@gmail.con>
2025-04-04 21:27:35 +0500

    typescript nima

---
+++ app.js
@@ -0,0 +1,4 @@
+"use strict";
+let fullName = "John Doe"; // This is a string
+let age = 30; // This is a number
+let isEmployed = true; // This is a boolean
---
+++ app.ts
@@ -0,0 +1,4 @@
+let fullName: string = "John Doe"; // This is a string
+let age: number = 30; // This is a number
+let isEmployed: boolean = true; // This is a boolean
+
