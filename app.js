febbd8a84ce84c75af53d0db093adea057108a3c
ZoyidjonNasretdinov <zoyidjonnasretdinovcoder@gmail.con>
2025-04-10 20:21:30 +0500

    TypeScript assosiy type

--- app.js
+++ app.js
@@ -2,3 +2,8 @@
 let fullName = "John Doe"; // This is a string
 let age = 30; // This is a number
 let isEmployed = true; // This is a boolean
+const salary = 50000; // This is a constant number
+const bonus = 5000; // This is a constant number
+const totalSalary = salary + bonus; // This is a constant number
+
+console.log(totalSalary); // This is a string
--- app.ts
+++ app.ts
@@ -2,3 +2,9 @@ let fullName: string = "John Doe"; // This is a string
 let age: number = 30; // This is a number
 let isEmployed: boolean = true; // This is a boolean
 
+
+const salary: number = 50000; // This is a constant number
+const bonus : number = 5000; // This is a constant number
+const totalSalary: number = salary + bonus; // This is a constant number
+
+console.log(totalSalary); // This is a string
